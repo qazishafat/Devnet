7c42e69a4a149cdcbaf803614fc77a3c80322570
qazishafat <qazishafat@gmail.com>
2023-02-02 23:38:35 -0500

    ccie script

---
+++ ribdump.py
@@ -0,0 +1,10 @@
+import sys
+import cli
+
+vrf = cli.execute('who vrf brief | in ipv')
+for vrfline in vrflines.split():
+	vrflines = vrf.splitlines()
+		if '4' in vrflines[2]:
+			print(cli.execute('show ip route ' +vrflines[0]
+		if '6' in vrflines[2]
+			print((cli.execute('show ipv6 route ' +vrflines[0] 
